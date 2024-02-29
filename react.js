66fa3effef367a9d2d82415332568b67172dc5df
SUBHIN-TM <subhintm2018@gmail.com>
2024-02-29 10:29:10 +0530

    created nested div

--- react.js
+++ react.js
@@ -1,9 +1,9 @@
-{/* <div id="parent">
-<div id="child 1"></div>
-<div id="child 2"></div>
-</div> */}
+/* <div id="parent">
+<div id="child 1">c1</div>
+<div id="child 2">c2</div>
+</div> */
 
 
-const heading= React.createElement("h1",{},"hello world from react")
+const heading= React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child 1"},"c1"),React.createElement("div",{id:"child 2"},"c2")])
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading)
