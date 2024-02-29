dcbe6a5cb9699e6bd59c9705b43d459179edf134
SUBHIN-TM <subhintm2018@gmail.com>
2024-02-29 11:00:23 +0530

    multiple div

--- react.js
+++ react.js
@@ -1,9 +1,16 @@
 /* <div id="parent">
 <div id="child 1">c1</div>
 <div id="child 2">c2</div>
-</div> */
+</div> 
+<div id="parent">
+<div id="child 1">c1</div>
+<div id="child 2">c2</div>
+</div> 
+*/
 
 
-const heading= React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child 1"},"c1"),React.createElement("div",{id:"child 2"},"c2")])
+const heading= [React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child 1"},"c1"),React.createElement("div",{id:"child 2"},"c2")]),
+               React.createElement("div",{id:"parent2"},[React.createElement("div",{id:"child 3"},"c1"),React.createElement("div",{id:"child 4"},"c2")])]
+
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading)
